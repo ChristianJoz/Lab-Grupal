e7a3251fbb8c1caee03954c1bc561f784557c002
Carlos-privado <65366496+Carlos-privado@users.noreply.github.com>
2020-10-05 15:56:48 -0600

    Splash , inicio , primer formulario

---
+++ app/src/main/java/com/example/labgrupal/Bienvenido.java
@@ -0,0 +1,28 @@
+package com.example.labgrupal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Bienvenido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenido);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), FormularioProducto.class);
+                startActivityForResult(intent, 0);
+            }
+
+        });
+        }
+
+}
