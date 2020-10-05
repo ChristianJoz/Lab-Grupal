beec5486898a0bdadb8d3b2e30f7d369a12adb00
ChristianJoz <65049993+ChristianJoz@users.noreply.github.com>
2020-10-05 17:24:54 -0600

    Formularios  e iconos

--- app/src/main/java/com/example/labgrupal/MainActivity.java
+++ app/src/main/java/com/example/labgrupal/MainActivity.java
@@ -41,13 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+
+        if (id == R.id.formulario_producto) {
+
+        }if(id == R.id.formulario_categoria) {
+            return true;
+        }if(id == R.id.formulario_usuario) {
             return true;
         }
 
--- app/src/main/java/com/example/labgrupal/formularioCategoria.java
+++ app/src/main/java/com/example/labgrupal/formularioCategoria.java
@@ -2,21 +2,13 @@ package com.example.labgrupal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-public class FormularioProducto extends AppCompatActivity {
+public class formularioCategoria extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario_producto);
+        setContentView(R.layout.activity_formulario_categoria);
     }
-
-
-
-
-
-
 }
